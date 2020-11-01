dd98eaf806608cc922efeee8ad35b076c05e92a6
Cristian Toma <cristi@tom.ro>
2020-11-01 11:50:27 +0200

    calculator response

--- src/Interface/Response/CalculateResponse.js
+++ src/Interface/Response/CalculateResponse.js
@@ -1,3 +1,5 @@
+import NumericField from "~/Interface/FieldType/NumericField";
+
 class CalculateResponse {
     isSuccess = false;
     statusCode = 0;
@@ -16,7 +18,53 @@ class CalculateResponse {
         response.statusCode = statusCode;
         response.isSuccess = true;
         response.payload = {
-            result: JSON.parse(JSON.stringify(calculatorResultDto)),
+            result: {
+                soilResistivity: {
+                    value: calculatorResultDto.soilResistivity,
+                    label: 'Rezistivitatea solului',
+                    unit: 'ohmi.cm',
+                },
+                verticalElectrodeDispersionResistance: {
+                    value: calculatorResultDto.verticalElectrodeDispersionResistance,
+                    label: 'Rez. de dispersie a el. vertical',
+                    unit: 'ohmi',
+                },
+                distanceBetweenVerticalElectrodesCM: {
+                    value: calculatorResultDto.distanceBetweenVerticalElectrodesCM,
+                    label: 'Distanta dintre electrozi',
+                    unit: 'cm',
+                },
+                verticalGroundingUF: {
+                    value: calculatorResultDto.verticalGroundingUF,
+                    label: 'Factor utilizare priza verticala',
+                    unit: '',
+                },
+                verticalGroundingResistance: {
+                    value: calculatorResultDto.verticalGroundingResistance,
+                    label: 'Rezistenta prizei verticale',
+                    unit: 'ohmi',
+                },
+                horizontalElectrodeDispersionResistance: {
+                    value: calculatorResultDto.horizontalElectrodeDispersionResistance,
+                    label: 'Rez. de dispersie el. orizontal',
+                    unit: 'ohmi',
+                },
+                horizontalGroundingUF: {
+                    value: calculatorResultDto.horizontalGroundingUF,
+                    label: 'Factor utiliz. priza orizontala',
+                    unit: '',
+                },
+                horizontalGroundingResistance: {
+                    value: calculatorResultDto.horizontalGroundingResistance,
+                    label: 'Rezistenta prizei orizontale',
+                    unit: 'ohmi',
+                },
+                multipleGroundingResistance: {
+                    value: calculatorResultDto.multipleGroundingResistance,
+                    label: 'Rezistenta prizei multiple',
+                    unit: 'ohmi',
+                },
+            },
         };
         
         return response;
